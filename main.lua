d7e1cc4c90c550488567aed9f9474de7f4d89dc5
Krynttt <harithxx123@gmail.com>
2026-02-19 01:26:19 +0800

    Implement cheat menu with button and slider
    Added a cheat menu with a button and a slider for walkspeed.

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
+-- This is your actual cheat menu
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Krynt Hub | Tap Simulator", "BloodTheme")
 
+local Main = Window:NewTab("Main")
+local Section = Main:NewSection("Tap Simulator Cheats")
+
+Section:NewButton("Auto Tap (Coming Soon)", "Feature in progress", function()
+    print("Button clicked!")
+end)
+
+Section:NewSlider("Walkspeed", "Speed up", 200, 16, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
